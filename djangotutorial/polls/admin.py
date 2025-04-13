58178fc27b57b05c67ef07a5f7912f2a43ef173c
cari395 <carinimatu11@live.com.ar>
2025-04-13 20:11:15 -0300

    Parte 3.3: Se agregaron nuevas funciones al admin y se modifico el template detail

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
+# Inline para Choice
+class ChoiceInline(admin.TabularInline):  # también podés usar StackedInline
+    model = Choice
+    extra = 3  # cantidad de opciones vacías por defecto
+
+# Configuración del admin de Question
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+# Registro
+admin.site.register(Question, QuestionAdmin)
