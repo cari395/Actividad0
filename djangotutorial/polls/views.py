de9b270e4210dd7a9ae0d2e1b56373620cedee13
cari395 <carinimatu11@live.com.ar>
2025-04-13 19:49:21 -0300

    Parte 3.1: Se agregaron nuevas vistas

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("Hola,estas en el index de encuestas")
+
+def detail(request, question_id):
+    return HttpResponse("Estas viendo la pregunta %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s." % question_id)
